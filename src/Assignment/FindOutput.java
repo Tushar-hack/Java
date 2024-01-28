c9b3c14c5626945dcb52a80701aeee12b77b007e
tushartak <tushartak861@gmail.com>
2024-01-28 21:50:26 +0530

    Added Find the Output in Assignment

---
+++ src/Assignment/FindOutput.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+
+// Guess the output of this program for each variable with explanation
+public class FindOutput {
+    private int y;
+    private static int z;
+    public static void main(String[] args) {
+        int x;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+}
---
+++ src/Learning/WrapperClasses.java
@@ -0,0 +1,9 @@
+package Learning;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        for (Integer i1 =0;i1<=10;i1++) {
+            System.out.println(i1);
+        }
+    }
+}
