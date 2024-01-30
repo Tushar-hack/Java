9973ee98f49fd305b9817f0f460dc4d5609e8bd6
tushartak <tushartak861@gmail.com>
2024-01-30 18:20:03 +0530

    Done for Today

---
+++ src/Learning/CliArguments.java
@@ -0,0 +1,9 @@
+package Learning;
+
+public class CliArguments {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("First Argument: " + arg);
+        }
+    }
+}
